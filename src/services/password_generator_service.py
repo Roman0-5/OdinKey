d85ae56436e989a587d6dc25f525c2ddba02d7a8
Matthias Bichler <matthias.bichler@stud.hcw.ac.at>
2025-11-29 18:09:45 +0100

    Passwort-Generator implementiert

--- src/services/password_generator_service.py
+++ src/services/password_generator_service.py
@@ -1,1 +1,43 @@
+import secrets
+import string
+
 class PasswordGeneratorService:
+
+    def generate_password(self,
+                          length: int = 12,
+                          use_symbols: bool = True,
+                          use_lower_case: bool = True,
+                          use_upper_case: bool = True,
+                          use_digits: bool = True) -> str:
+
+        # pool mit allen zulässigen Zeichen erstellen
+        pool = ""
+        length = int(length)
+        if use_symbols:
+            pool += string.punctuation
+        if use_lower_case:
+            pool += string.ascii_lowercase
+        if use_upper_case:
+            pool += string.ascii_uppercase
+        if use_digits:
+            pool += string.digits
+
+        # Fehler werfen, wenn nichts ausgewählt wurde
+        if pool == "":
+            raise ValueError ("Bitte mindestens einen Parameter wählen")
+
+        password = ""
+        for i in range(length):
+            password += str(secrets.choice(pool))
+
+        return password
+
+
+
+
+
+
+
+
+
+
